c6ef085b25e9ce08e19c6cb6e6ce6a328c28bb6f
jordank7771 <king690@purdue.edu>
2026-02-26 14:15:54 -0500

    finished lab

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import pygame as pg
 
 pg.init()
+pg.display.set_caption("CGT115 Lab 5 - Compositing")
 
-pg.display.set_caption("Hello World")
+#Load images
+img = pg.image.load("img/backgrounds/winter.png")
+img2 = pg.image.load("img/characters/yoda.png")
 
-img = pg.image.load("C:\Users\spagh\OneDrive\Desktop\images1\backgrounds\winter.png")
-img2 = pg.image.load("C:\Users\spagh\OneDrive\Desktop\images1\characters\yoda.png")
-
+#Set screen
 width = img.get_width()
 height = img.get_height()
 screen = pg.display.set_mode((width, height))
+
+#Pixel color to be replaced
+baseline_color = img2.get_at((0,0))
+
 for y in range(0, height):
     for x in range(0, width):
+        #Get pixel from image
         c1 = img.get_at((x, y))
         c2 = img2.get_at((x, y))
-        newColor = ((c1.r + c2.r) // 2, (c1.g + c2.g) // 2, (c1.b + c2.b) // 2)
-        img.set_at((x, y), newColor)
+
+        #Checks for green pixel
+        if c2 == baseline_color:
+            #Sets img as new pixel
+            newColor = img.get_at((x, y))
+            img.set_at((x, y), newColor)
+        else:
+            #Keeps img2 pixel
+            newColor = img2.get_at((x, y))
+            img.set_at((x, y), c2)
 
 screen.blit(img, (0,0))
 
 pg.display.flip()
 
-pg.image.save(img, "BW.png")
+#File save
+pg.image.save(img, "mergedImg.png")
 
+#Run
 done = False
 while not done:
     for event in pg.event.get():
